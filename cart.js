cc9bc3824516c43e0cb5064268e4f88d47064515
Arun <arunsingh641997@gmail.com>
2022-10-03 19:51:11 +0530

    Cart Modified

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
- // ************************************************
+// ************************************************
 // Shopping Cart API
 // ************************************************
 
@@ -154,25 +154,38 @@ var shoppingCart = (function() {
     displayCart();
   });
   
-  
+  function empty(){
+    let sadCart= document.getElementById("empCart");
+    sadCart.className="gayab";
+  }
+  function dikhja(){
+    let sadCart1= document.getElementById("empCart");
+    sadCart1.className="dikhja";
+  }
+
   function displayCart() {
     var cartArray = shoppingCart.listCart();
     var output = "";
     for(var i in cartArray) {
       output += "<tr>"
-        + "<td>" + cartArray[i].name + "</td>" 
-        + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td>"  + cartArray[i].name + "</td>" 
+        + "<td>" + "&#8377;" + cartArray[i].price + "</td>"
         + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
         + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
         + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
         + " = " 
-        + "<td>" + cartArray[i].total + "</td>" 
+        + "<td>"+ "&#8377;" + cartArray[i].total + "</td>" 
         +  "</tr>";
     }
     $('.show-cart').html(output);
     $('.total-cart').html(shoppingCart.totalCart());
     $('.total-count').html(shoppingCart.totalCount());
+    if(shoppingCart.totalCount()==0){
+      dikhja();
+    }else{
+      empty();
+    }
   }
   
   // Delete item button
@@ -200,10 +213,11 @@ var shoppingCart = (function() {
   // Item count input
   $('.show-cart').on("change", ".item-count", function(event) {
      var name = $(this).data('name');
-     var count = Number($(this).val());
+      count = Number($(this).val());
     shoppingCart.setCountForItem(name, count);
     displayCart();
   });
+
   
-  displayCart();
+  displayCart();  
   
