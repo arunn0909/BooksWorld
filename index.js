edfe35dcb392cacf7453d300a436fa509d72cc94
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2022-10-05 23:36:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,16 @@ const swiper = new Swiper('.swiper', {
 /* Slider Part Js ends */
 
 const body = document.querySelector("body");
+const genre = document.querySelector("#genre");
 const toggle = document.querySelector("#toggle");
 const sunIcon = document.querySelector(".toggle .bxs-sun");
 const moonIcon = document.querySelector(".toggle .bx-moon");
 
+
 toggle.addEventListener("change", () => {
     
     body.classList.toggle("dark");
+    genre.classList.toggle("dark");
     sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
     moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
 
