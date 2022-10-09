f2ddbb7b6928cd67a218cc6e6c040fa12508f522
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2022-10-09 15:20:32 +0530

    responsive cart

--- cart.js
+++ cart.js
@@ -168,15 +168,16 @@ var shoppingCart = (function() {
     var output = "";
     for(var i in cartArray) {
       output += "<tr>"
-        + "<td>"  + cartArray[i].name + "</td>" 
-        + "<td>" + "&#8377;" + cartArray[i].price + "</td>"
+        + "<td>" + cartArray[i].name
+        + "<span class='cost'>" + "&#8377;" + cartArray[i].price + "</span>"
+        + "</td>" 
         + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
         + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
         + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
         + " = " 
-        + "<td>"+ "&#8377;" + cartArray[i].total + "</td>" 
-        +  "</tr>";
+        + "<td class='cost'>" + "&#8377;" + cartArray[i].total + "</td>" 
+        + "</tr>";
     }
     $('.show-cart').html(output);
     $('.total-cart').html(shoppingCart.totalCart());
