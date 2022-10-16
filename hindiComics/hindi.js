f0f5eb1461f634deba3075cc5337360697651a63
Ajay <lci2021023@iiitl.ac.in>
2022-10-16 11:23:57 +0530

    Fixed Dark mode for all pages

--- hindiComics/hindi.js
+++ hindiComics/hindi.js
@@ -1,6 +1,9 @@
 // Check theme and assign Dark mode or Light mode
 
+
 const selectedTheme = document.cookie;
+
+// console.log(selectedTheme);
 
 if (document.cookie == "light")
     document.body.style.background = "white";
